8f2641b37371fd2859217b1ee6d32603963adbda
Daniel Coupal <cm@universia.com>
2013-06-27 08:27:37 -0700

    Default to localhost for the Mongo connection

--- src/apps/testmgo/testmgo.go
+++ src/apps/testmgo/testmgo.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-    //"encoding/json"
-   "fmt"
+	//"encoding/json"
+	"fmt"
 	"os"
 )
 
 import (
-    "labix.org/v2/mgo"
-    "labix.org/v2/mgo/bson"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
 
 func main() {
-    var doc map[string]interface{}
-    session, err := mgo.Dial("apollo13.local:27017")  //debugger
-    if err != nil {
-        fmt.Println("Can't connect to host")
-        panic(err)
-    }
-    defer session.Close()
-    collCon := session.DB("log").C("acme_usage")
-    iter := collCon.Find(bson.M{}).Iter()
-    for iter.Next(&doc) {
-        // Send to worker for validation
-        fmt.Printf("_id: %s\n", doc["_id"])	//debugger
-        fmt.Printf("user: %s\n", doc["user"])	//debugger
-    }
+	var doc map[string]interface{}
+	session, err := mgo.Dial("localhost:27017") //debugger
+	if err != nil {
+		fmt.Println("Can't connect to host")
+		panic(err)
+	}
+	defer session.Close()
+	collCon := session.DB("log").C("acme_usage")
+	iter := collCon.Find(bson.M{}).Iter()
+	for iter.Next(&doc) {
+		// Send to worker for validation
+		fmt.Printf("_id: %s\n", doc["_id"])   //debugger
+		fmt.Printf("user: %s\n", doc["user"]) //debugger
+	}
 
 	os.Exit(0)
 }
